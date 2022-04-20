23761f2de590c916658f9610c3f1d343a3fe69d3
Mads Bram Cordes <Mobilpadde@gmail.com>
2022-04-20 15:18:01 +0200

    Hasty

---
+++ _data/sites/hasty.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "Hasty",
+  description: "Hasty tests",
+  options: {
+    frequency: 60 * 23, // (in minutes), 23 hours
+  },
+  urls: [
+    "https://hasty.dev/",
+    "https://hasty.dev/new",
+    "https://hasty.dev/page",
+    "https://hasty.dev/shorts",
+    "https://hasty.dev/shorts/filtering",
+    "https://hasty.dev/shorts/bubble-vs-selection",
+  ],
+};
